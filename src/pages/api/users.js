53214a6f4caff701beae34c5df9d1b99cee85bcc
DongKyu <po4tion0429@gmail.com>
2021-10-20 03:05:42 +0900

    #7 settings absolute import

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -1,5 +1,5 @@
-import dbConnect from '../../lib/dbConnect';
-import User from '../../models/User';
+import dbConnect from 'lib/dbConnect';
+import User from 'models/User';
 
 export default async function handler(req, res) {
 	const { method } = req;
