c5493c403c30e7ec187dc9cfa19a3c2292532a9c
DongKyu <po4tion0429@gmail.com>
2021-10-26 01:49:04 +0900

    #17 signup using material-ui yup react-hook-form

--- pages/signup.js
+++ pages/signup.js
@@ -1,9 +1,130 @@
-function Signin() {
+import * as React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { signupValidation } from '/lib';
+
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+function Signup() {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(signupValidation),
+	});
+
+	const onSubmit = data => {
+		console.log(data);
+	};
+
 	return (
-		<div>
-			<p>하이</p>
-		</div>
+		<Container component="main" maxWidth="xs">
+			<CssBaseline />
+			<Box
+				sx={{
+					marginTop: 8,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+				}}
+			>
+				<Typography component="h1" variant="h5">
+					회원가입
+				</Typography>
+				<Box sx={{ mt: 3 }}>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<TextField
+								autoFocus
+								fullWidth
+								id="name"
+								label="Name"
+								name="name"
+								required
+								type="text"
+								{...register('name')}
+								error={errors.name ? true : false}
+							/>
+							<Typography variant="inherit" color="error">
+								{errors.name?.message}
+							</Typography>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								fullWidth
+								id="email"
+								label="Email Address"
+								name="email"
+								required
+								type="email"
+								{...register('email')}
+								error={errors.email ? true : false}
+							/>
+							<Typography variant="inherit" color="error">
+								{errors.email?.message}
+							</Typography>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								fullWidth
+								id="password"
+								label="Password"
+								name="password"
+								required
+								type="password"
+								{...register('password')}
+								error={errors.password ? true : false}
+							/>
+							<Typography variant="inherit" color="error">
+								{errors.password?.message}
+							</Typography>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								fullWidth
+								id="confirmPassword"
+								label="ConfirmPassword"
+								name="confirmPassword"
+								required
+								type="password"
+								{...register('confirmPassword')}
+								error={errors.confirmPassword ? true : false}
+							/>
+							<Typography variant="inherit" color="error">
+								{errors.confirmPassword?.message}
+							</Typography>
+						</Grid>
+					</Grid>
+					<Button
+						onClick={handleSubmit(onSubmit)}
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+					>
+						가입하기
+					</Button>
+					<Grid container justifyContent="flex-end">
+						<Grid item>
+							<Link href="/signin" variant="body2">
+								계정이 있으시다면, 로그인 해주세요
+							</Link>
+						</Grid>
+					</Grid>
+				</Box>
+			</Box>
+		</Container>
 	);
 }
 
-export default Signin;
+export default Signup;
