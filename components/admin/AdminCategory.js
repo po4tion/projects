460a98046e4aa071ba96191b5f91a3a06b57c162
DongKyu <po4tion0429@gmail.com>
2021-11-04 04:25:01 +0900

    #32 category remove

--- components/admin/AdminCategory.js
+++ components/admin/AdminCategory.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
-import { createCategory, getCategories } from '/actions/handleCategory';
+import {
+	createCategory,
+	getCategories,
+	removeCategory,
+} from '/actions/handleCategory';
 import { getCookie } from '/actions/handleAuth';
-import Router from 'next/router';
 
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
@@ -29,12 +32,11 @@ function AdminCategory({ accessToken }) {
 		name: '',
 		error: '',
 		success: '',
-		removed: '',
 		reload: false,
 		loading: false,
 	});
 	const [categories, setCategories] = useState(null);
-	const { name, error, success, removed, reload, loading } = info;
+	const { name, error, success, reload, loading } = info;
 
 	// 카테고리 리스트 불러오기
 	useEffect(() => {
@@ -84,98 +86,141 @@ function AdminCategory({ accessToken }) {
 					loading: false,
 					reload: !reload,
 				});
+			}
+		});
+	};
 
-				// Router.reload(window.location.pathname);
+	const deleteCtgCheck = (name, slug) => {
+		const result = window.confirm(`${name} 카테고리를 삭제하시겠습니까?`);
+
+		if (result) {
+			deleteCategory(name, slug);
+		}
+	};
+
+	const deleteCategory = async (name, slug) => {
+		setInfo({
+			...info,
+			loading: true,
+		});
+
+		await removeCategory(slug, token).then(data => {
+			if (data.error) {
+				setInfo({
+					...info,
+					error: `${name} 카테고리 삭제가 불가능합니다`,
+					success: false,
+					loading: false,
+				});
+			} else {
+				setInfo({
+					...info,
+					error: false,
+					success: `${name} 카테고리가 삭제되었습니다`,
+					loading: false,
+					reload: !reload,
+				});
 			}
 		});
 	};
 
 	return (
 		<>
-			<Container component="main" maxWidth="xs">
+			<Container component="main" maxWidth="md">
 				<CssBaseline />
-				<Box
-					sx={{
-						marginTop: 8,
-						display: 'flex',
-						flexDirection: 'column',
-						alignItems: 'center',
-					}}
-				>
-					<Box component="form" noValidate sx={{ width: '100%', mt: 1 }}>
-						<Grid item xs={12} sx={{ textAlign: 'center' }}>
-							{loading && <CircularProgress />}
-						</Grid>
-						<Grid container>
-							<Grid item xs={12}>
-								<TextField
-									value={name}
-									margin="normal"
-									required
-									fullWidth
-									id="category"
-									label="카테고리 입력"
-									name="category"
-									autoComplete="category"
-									autoFocus
-									onChange={handleChange}
-									error={info.error ? true : false}
-								/>
-							</Grid>
-							{info.error && (
-								<Typography variant="inherit" color="error">
-									{info.error}
-								</Typography>
-							)}
-							{info.success && (
-								<Typography variant="inherit" color="primary">
-									{info.success}
-								</Typography>
-							)}
-						</Grid>
-						<Button
-							onClick={handleSubmit}
-							fullWidth
-							variant="contained"
-							sx={{ mt: 3, mb: 2 }}
+				<Grid container spacing={2}>
+					<Grid item xs={6}>
+						<Box
+							sx={{
+								marginTop: 8,
+								display: 'flex',
+								flexDirection: 'column',
+								alignItems: 'center',
+							}}
 						>
-							추가하기
-						</Button>
-					</Box>
-				</Box>
-				<Box
-					sx={{
-						marginTop: 8,
-						display: 'flex',
-						flexDirection: 'column',
-						alignItems: 'center',
-					}}
-				>
-					<List
-						sx={{
-							width: '100%',
-							maxWidth: 360,
-							bgcolor: 'background.paper',
-							position: 'relative',
-							overflow: 'auto',
-							maxHeight: 300,
-							'& ul': { padding: 0 },
-						}}
-					>
-						{categories &&
-							categories.map(ctg => (
-								<li key={`${ctg._id}`}>
-									<ul>
-										<ListItem divider disablePadding>
-											<ListItemButton onDoubleClick={() => console.log('hi')}>
-												<ListItemText primary={`${ctg.name}`} />
-											</ListItemButton>
-										</ListItem>
-									</ul>
-								</li>
-							))}
-					</List>
-				</Box>
+							<Box component="form" noValidate sx={{ width: '100%', mt: 1 }}>
+								<Grid item xs={12} sx={{ textAlign: 'center' }}>
+									{loading && <CircularProgress />}
+								</Grid>
+								<Grid container>
+									<Grid item xs={12}>
+										<TextField
+											value={name}
+											margin="normal"
+											required
+											fullWidth
+											id="category"
+											label="카테고리 입력"
+											name="category"
+											autoComplete="category"
+											autoFocus
+											onChange={handleChange}
+											error={error ? true : false}
+										/>
+									</Grid>
+									{error && (
+										<Typography variant="inherit" color="error">
+											{error}
+										</Typography>
+									)}
+									{success && (
+										<Typography variant="inherit" color="primary">
+											{success}
+										</Typography>
+									)}
+								</Grid>
+								<Button
+									onClick={handleSubmit}
+									fullWidth
+									variant="contained"
+									sx={{ mt: 3, mb: 2 }}
+								>
+									추가하기
+								</Button>
+							</Box>
+						</Box>
+					</Grid>
+					<Grid item xs={6}>
+						<Box
+							sx={{
+								width: '100%',
+								marginTop: 8,
+								display: 'flex',
+								flexDirection: 'column',
+								alignItems: 'center',
+							}}
+						>
+							<List
+								sx={{
+									width: '100%',
+									maxWidth: 360,
+									bgcolor: 'background.paper',
+									position: 'relative',
+									overflow: 'auto',
+									maxHeight: 300,
+									'& ul': { padding: 0 },
+								}}
+							>
+								{categories &&
+									categories.map(ctg => (
+										<li key={`${ctg._id}`}>
+											<ul>
+												<ListItem divider disablePadding>
+													<ListItemButton
+														onDoubleClick={() =>
+															deleteCtgCheck(ctg.name, ctg.slug)
+														}
+													>
+														<ListItemText primary={`${ctg.name}`} />
+													</ListItemButton>
+												</ListItem>
+											</ul>
+										</li>
+									))}
+							</List>
+						</Box>
+					</Grid>
+				</Grid>
 			</Container>
 		</>
 	);
