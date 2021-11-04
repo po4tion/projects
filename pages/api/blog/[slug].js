ed80218b4ad7a352bf06e42382eb99bb1b672571
DongKyu <po4tion0429@gmail.com>
2021-11-05 04:54:06 +0900

    #37 delete one blog api

--- pages/api/blog/[slug].js
+++ pages/api/blog/[slug].js
@@ -43,6 +43,39 @@ export default function handler(req, res) {
 					return res.status(400).json({ error: '에러' });
 				}
 				break;
+			case 'DELETE':
+				try {
+					// 토큰 유효성 검사
+					const user = await tokenValidation(req, res).catch(msg =>
+						res.status(400).json({ error: 'getTokenError' })
+					);
+
+					// 유효성 검사를 통과하고 받은 데이터 전송 후, 프로필 값 반환
+					const auth = await adminMiddleware(req, res, user);
+
+					req.profile.password = undefined;
+
+					if (auth) {
+						const { slug } = req.query;
+
+						await Blog.findOneAndRemove({ slug: slug.toLowerCase() }).exec(
+							(err, data) => {
+								if (err) {
+									return res.status(400).json({
+										error: '해당 블로그 삭제를 실패했습니다',
+									});
+								}
+
+								return res
+									.status(200)
+									.json({ success: '해당 블로그를 삭제했습니다.' });
+							}
+						);
+					}
+				} catch (error) {
+					return res.status(400).json({ error: '에러' });
+				}
+				break;
 			default:
 				return res.status(400).json({ error: 'request method를 확인해주세요' });
 				break;
