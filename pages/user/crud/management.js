37a7ad2c01a4de7b22602f4c9ca4976c3188e0cb
DongKyu <po4tion0429@gmail.com>
2021-11-18 10:53:06 +0900

    # find error...

--- pages/user/crud/management.js
+++ pages/user/crud/management.js
@@ -5,10 +5,11 @@ import { getUserProfile } from '/actions/handleUser';
 import { ManagementBlog } from '/components/blog';
 import { ProtectRoute } from '/components/auth';
 
-function Management({ token, profile, blogList, size }) {
+function Management({ token, profile }) {
 	return (
 		<ProtectRoute>
-			<ManagementBlog blogList={blogList} token={token} size={size} />
+			{/* <ManagementBlog blogList={blogList} token={token} size={size} /> */}
+			{JSON.stringify(profile)}
 		</ProtectRoute>
 	);
 }
@@ -19,15 +20,11 @@ export async function getServerSideProps(ctx) {
 	const { req } = ctx;
 	const accessToken = req.headers.cookie.slice(13);
 	const profile = await getUserProfile(accessToken);
-	const blogList = await getBlogsInServer(encodeURI('포션'));
-	const size = await blogList.data.length;
 
 	return {
 		props: {
 			token: accessToken,
 			profile,
-			blogList,
-			size,
 		},
 	};
 }
