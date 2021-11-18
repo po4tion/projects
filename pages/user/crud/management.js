075fafe4eea6671bda2c6d3eee9c384347f842c6
DongKyu <po4tion0429@gmail.com>
2021-11-18 10:49:54 +0900

    # check bug

--- pages/user/crud/management.js
+++ pages/user/crud/management.js
@@ -5,14 +5,10 @@ import { getUserProfile } from '/actions/handleUser';
 import { ManagementBlog } from '/components/blog';
 import { ProtectRoute } from '/components/auth';
 
-function Management({ token, profile, blogList }) {
+function Management({ token, profile, blogList, size }) {
 	return (
 		<ProtectRoute>
-			<ManagementBlog
-				blogList={blogList}
-				token={token}
-				size={blogList.data.length}
-			/>
+			<ManagementBlog blogList={blogList} token={token} size={size} />
 		</ProtectRoute>
 	);
 }
@@ -24,12 +20,14 @@ export async function getServerSideProps(ctx) {
 	const accessToken = req.headers.cookie.slice(13);
 	const profile = await getUserProfile(accessToken);
 	const blogList = await getBlogsInServer(encodeURI('포션'));
+	const size = await blogList.data.length;
 
 	return {
 		props: {
 			token: accessToken,
 			profile,
 			blogList,
+			size,
 		},
 	};
 }
