096d60aa93fb1b1cd1a10cff37e4902b92f476ad
Donggyu Kim <55759551+po4tion@users.noreply.github.com>
2022-04-21 09:59:46 +0900

    Revert "Feature"

--- atoms/atom.userInfo.ts
+++ atoms/atom.userInfo.ts
@@ -1,23 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { atom } from "recoil";
 
-export const globalState = atom<any>({
-  key: "globalState",
-  default: null,
-});
-
-export const realtimeState = atom<any>({
-  key: "realtimeState",
-  default: null,
-});
-
-export const legendsState = atom<any>({
-  key: "legendsState",
-  default: null,
-});
-
-export const clubState = atom<any>({
-  key: "clubState",
+export const userState = atom<any>({
+  key: "apexUserState",
   default: null,
 });
 
